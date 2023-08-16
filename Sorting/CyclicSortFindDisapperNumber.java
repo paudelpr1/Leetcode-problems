906d5c92c9a9abc8685e90020b3ddb44fdd78386
paudelpr1 <prasanshapaudel111@gmail.com>
2023-08-16 12:05:34 -0400

    Leetcode 448

---
+++ Sorting/CyclicSortFindDisapperNumber.java
@@ -0,0 +1,52 @@
+package Sorting;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CyclicSortFindDisapperNumber {
+    public static void main(String[] args){
+        int[] arr = {4,3,2,7,8,2,3,1};
+        List<Integer> result = new ArrayList<Integer>();
+        cyclicSort(arr);
+        result = findDisappearedNumbers(arr);
+        System.out.println(result.toString());
+
+    }
+
+    static List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> result = new ArrayList<Integer>();
+        int i = 0;
+        while(i < nums.length)
+        {
+            if(nums[i] == i+1)
+            {
+                i++;
+            }
+            else{
+                result.add(i+1);
+                i++;
+            }
+        }
+        return result;
+        
+    }
+
+    static void cyclicSort(int[] nums){
+        int i = 0; 
+        while(i < nums.length){
+            int index = nums[i] - 1; 
+            if(nums[i] != nums[index]){
+                swap(nums, i , index);
+            }
+            else{
+                i++;
+            }
+        }
+    }
+
+    static void swap(int[] arr, int first, int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
