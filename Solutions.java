b3a5716f61c25ef7633849051b6be9293763cc54
Prasansha Paudel <94033599+paudelpr1@users.noreply.github.com>
2024-02-25 11:57:58 -0500

    Create Solutions.java
    Design an algorithm to encode a list of strings to a string. The encoded string is then sent over the network and is decoded back to the original list of strings.

---
+++ Solutions.java
@@ -0,0 +1,46 @@
+import java.util.*;
+import java.lang.*; 
+
+public class Solution {
+    /*
+     * @param strs: a list of strings
+     * @return: encodes a list of strings to a single string.
+     */
+    public static void main(String[] args) {
+        List<String> strs = new ArrayList<>();
+        strs.add("How");
+        strs.add("Now");
+        strs.add("Brown");
+        strs.add("Cow");
+        StringBuilder res = new StringBuilder();
+        for(int i = 0; i < strs.size(); i++){
+            res.append(strs.get(i).length());
+            res.append("#");
+            res.append(strs.get(i));
+        }
+        System.out.println(res);
+    }
+
+    /*
+     * @param str: A string
+     * @return: decodes a single string to a list of strings
+     */
+      public static void main(String[] args){
+        String str = "3#How3#Now5#Brown3#Cow";
+        int i = 0;
+        List<String> res = new ArrayList();
+        while(i < str.length()){
+            int j = i; 
+            int length = Integer.parseInt(str.substring(i,j+1));
+            while(str.charAt(j) != '#'){
+                j++;
+            }
+            i = j+1;
+            res.add(str.substring(i, i+length));
+            i = i+length;
+        }
+        
+        System.out.println(res);
+        
+    }
+}
