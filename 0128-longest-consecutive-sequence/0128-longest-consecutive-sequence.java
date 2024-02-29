1edb1c546c48fbe72ff69498b1e3ce88bd012a33
Prasansha Paudel <94033599+paudelpr1@users.noreply.github.com>
2024-02-28 20:33:53 -0500

    Time: 1148 ms (7.06%), Space: 66.2 MB (41.99%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,50 +1,26 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
-        if(nums.length == 0){
-            return 0;
-        }
-        Arrays.sort(nums);
-        int maxcount = 1; 
-        int count = 1;
-        int start = nums[0];
-        for(int i = 1; i < nums.length; i++){
-            if(nums[i] != start && nums[i] == (start+1)){
-                count++;
-            }
-            else if(nums[i] == start){
-                continue;
-            }
-            else{
-                maxcount = Math.max(maxcount, count);
-                count = 1;
-            }
-            start = nums[i];
-            
-        }
-        maxcount = Math.max(maxcount, count);
-        return maxcount;
+        if(nums.length == 0) return 0; 
 
+        Set<Integer> store = new HashSet();
+        for(int x: nums){
+            store.add(x);
+        }
+        int maxCount = 0; 
 
-        
+        for(int i = 0; i < nums.length; i++){
+            int num = nums[i];
+            int count = 0;
+            if(!store.contains(num-1)){
+                while(store.contains(num + count)){
+                    count++; 
+                }
+            }
+
+            maxCount = Math.max(maxCount, count);
+
+        }
+        return maxCount;
     }
-
-    // public static void swap(int[] arr, int first, int second)
-    // {
-    //     int temp = arr[second];
-    //     arr[second] = arr[first];
-    //     arr[first] = temp;
-    // }
-
-    // public static void Sort(int[] arr){
-    //     for(int i = 0; i< arr.length - 1; i++){
-    //         for(int j = i+1; j > 0; j--){
-    //             if(arr[j] < arr[j-1]){
-    //                 swap(arr, j, j-1);
-    //             }
-    //             else{
-    //                 break;
-    //             }
-    //         }
-    //     }
-    // }
+        
 }
